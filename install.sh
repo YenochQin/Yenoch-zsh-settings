b78d1365d57affc580792a9487ab4c1d7b27a09b
Qin Yi <yenochqin@hotmail.com>
2025-11-07 01:24:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ cp .zshrc ~
 
 cp yenoch.zsh-theme ~/.oh-my-zsh/themes
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 source ~/.zshrc
